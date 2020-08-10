7f74eddf4fe957999ea0cc751fe8af2d46746e9a
anakyouzogolov <64495296+anakyouzogolov@users.noreply.github.com>
2020-08-10 07:10:55 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,24 @@
+import whois
+import pprint
+import socket
+
+def get_info_by_domain(domain_name):
+
+    try:
+
+        ip_addr = socket.gethostbyname(domain_name)
+        print("ip address : ",ip_addr)
+
+        w = whois.whois(domain_name)
+
+        print(w)
+
+    except Exception as exp:
+        print(exp)
+
+ 
+my_domain = input("Enter a domain name : ")
+
+get_info_by_domain(my_domain)
+
+
